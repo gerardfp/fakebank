414a10621653d8e2758bc43e6e33ca3f0a2e4841
Gerard Falco <gerardfp@users.noreply.github.com>
2023-02-26 17:55:55 +0100

    Update get-interest.js

--- api/get-interest.js
+++ api/get-interest.js
@@ -1,3 +1,7 @@
+function sleep(milliseconds) {  
+    return new Promise(resolve => setTimeout(resolve, milliseconds));  
+}  
+
 export default async function handler(req, res) {
     await sleep(5000);  
     
