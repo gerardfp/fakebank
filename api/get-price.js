be9631cd70226ccf648cc3dc733eb2d76df6a324
Gerard Falco <gerardfp@users.noreply.github.com>
2023-03-16 11:41:43 +0100

    Update get-price.js

--- api/get-price.js
+++ api/get-price.js
@@ -1,9 +1,3 @@
-function sleep(milliseconds) {  
-    return new Promise(resolve => setTimeout(resolve, milliseconds));  
-}  
-
 export default async function handler(req, res) {
-    await sleep(5000);  
-    
     res.send(Math.random()*30);
 }
