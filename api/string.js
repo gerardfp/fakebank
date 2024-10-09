abdd77044c29e8f0dc56e3e3e9f1e1d604ea97c1
Gerard Falco <gerardfp@users.noreply.github.com>
2024-10-09 22:43:56 +0200

    .

--- api/string.js
+++ api/string.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
     let result = '';
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    for(let i = 0; i < characters.length; i++) {
+    for(let i = 0; i < 16; i++) {
         result += characters.charAt(Math.floor(Math.random() * characters.length));
     }
     res.send(result);
